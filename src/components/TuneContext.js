73c29ae1cd90e09b947e58a36893904d748a6991
arikscherm <arikscherm@gmail.com>
2024-12-18 14:16:21 -0700

    Add margin above heading

--- src/components/TuneContext.js
+++ src/components/TuneContext.js
@@ -3,7 +3,14 @@ import React, { createContext, useState } from 'react';
 export const TuneContext = createContext();
 
 export const TuneProvider = ({ children }) => {
-    const [tuneLibrary, setTuneLibrary] = useState(['Hunters House', 'Fred Finns', 'George Whites', 'Pigtown Fling', 'Tom Wards Downfall', 'Beare Island', 'Doctor Gilberts', 'Father Kellys', 'Mountain Road', 'Maud Miller', 'Yellow Tinker', 'Dowds no 9']);
+    const [tuneLibrary, setTuneLibrary] = useState(
+      [
+        'Tom Wards Downfall', 'George Whites Favorite', 'Fox on the Town', 'Green Mountain', 'Reconciliation', 'MacArthur Road', 
+        'Black Pats', 'Musical Priest', 'Green Groves of Erin', 'Sally Gardens', 'Maud Miller', 'The Mountain Road',
+        'Flooded Road to Glenties', 'Martin Wynnes #2', 'Star of Munster', 'Sweeneys Dream', 'Congress', 'Palmers Gate',
+        'Black Pats', 'Farrel O Gara', 'Concertina Reel', 'Beare Island', 'Monoghan Twig', 'Earls Chair',
+        'Julia Delaneys', 'Martin OConnors', 'Foxhunters', 'Father Kellys', 'Farewell to Erin', 'Castle Kellys',
+      ]);
     const [remainingTunes, setRemainingTunes] = useState([...tuneLibrary]);
     const [setsPlayed, setSetsPlayed] = useState([]);
     const [favoritesList, setFavoritesList] = useState([]);
