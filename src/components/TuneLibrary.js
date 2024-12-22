282ffe14dc52cfe8c20fe9161cc55085f5237b26
arikscherm <arikscherm@gmail.com>
2024-12-21 17:22:41 -0700

    Session API working

--- src/components/TuneLibrary.js
+++ src/components/TuneLibrary.js
@@ -5,7 +5,7 @@ import { TuneContext } from './TuneContext';
 function removeTune(tuneLibrary, setTuneLibrary, remainingTunes, setRemainingTunes, tuneToRemove) {
 
     const updatedTuneLibrary = tuneLibrary.filter(tune => tune !== tuneToRemove);
-    setTuneLibrary(updatedTuneLibrary)
+    setTuneLibrary(updatedTuneLibrary);
 
     if (remainingTunes.includes(tuneToRemove)) {
         const updatedRemainingTunes = remainingTunes.filter(tune => tune !== tuneToRemove);
@@ -32,29 +32,69 @@ function addTune(newTune, setNewTune, tuneLibrary, setTuneLibrary, setRemainingT
 }
 
 
+async function fetchFromSession(numTunes, setNumTunes, tuneLibrary, setTuneLibrary, remainingTunes, setRemainingTunes) {
+    const pages = Math.ceil(numTunes / 50);
+    let newTuneLibrary = [];
+    console.log(pages)
+    for (let i = 1; i < pages+1; i++) {
+        console.log("Before Fetch");
+        try {
+            const response = await fetch(`https://thesession.org/tunes/popular/reels?format=json&perpage=50&page=${i}`);
+            if (!response.ok) {
+                throw new Error('Unable to connect to the session');
+            }
+            const data = await response.json();
+            const fetchedTunes = data.tunes.map(tune => tune.name);
+            console.log(fetchedTunes);
 
-// fetch('https://thesession.org/tunes/popular/reels?format=json&perpage=50&page=1')
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error('Unable to connect to the session');
-//     }
-//     return response.json();
-//   })
-//   .then(data => {
-//     const tunes = data.tunes.map(tune => tune.name);
-//     console.log(tunes)
+            // If it's the first page, reset the tune library
+            if (i === 1) {
+                setTuneLibrary([]);
+                console.log("Tune Library Reset");
+            }
 
-//   })
-//   .catch(error => {
-//     console.error('Error:', error);
-//   });
+            // If it's the last page, handle remaining tunes
+            if (i === pages) {
+                console.log("Last page");
+                const remainingNumTunes = numTunes % 50;
+                if (remainingNumTunes !== 0) {
+                    newTuneLibrary = [...newTuneLibrary, ...fetchedTunes.slice(0, remainingNumTunes)];
+                }
+                else {
+                    newTuneLibrary = [...newTuneLibrary, ...fetchedTunes];
+                }
+                
+            }
+            else { 
+                newTuneLibrary = [...newTuneLibrary, ...fetchedTunes];
+            }
+
+
+
+        } catch (error) {
+            console.error('Processing Error:', error);
+        }
+    }
+    setNumTunes('');
+    setTuneLibrary(newTuneLibrary);
+    // const updatedRemainingTunes = tuneLibrary;
+    setRemainingTunes(newTuneLibrary);
+    console.log(tuneLibrary);
+}
+
 
 function TuneLibrary() {
     const { tuneLibrary, remainingTunes, setRemainingTunes, setTuneLibrary } = useContext(TuneContext);
     const [newTune, setNewTune] = useState('');
+    const [numTunes, setNumTunes] = useState('');
     const handleKeyDown = (e) => {
         if (e.key === 'Enter') {
             addTune(newTune, setNewTune, tuneLibrary, setTuneLibrary, setRemainingTunes, remainingTunes);
+        }
+    }
+    const handleKeyDownFetch = (e) => {
+        if (e.key === 'Enter') {
+            fetchFromSession(numTunes, setNumTunes, tuneLibrary, setTuneLibrary, remainingTunes, setRemainingTunes);
         }
     }
     return (
@@ -75,8 +115,8 @@ function TuneLibrary() {
             <h2>Or...</h2>
             <h4>Get the top XYZ most popular tunes</h4>
                 <h4>from <i><u>TheSession.org</u></i>.</h4>
-            <input placeholder="XYZ"/>
-            <button onClick={() => console.log("to the session!")}>Go!</button>
+            <input placeholder="XYZ" type="number" value={numTunes} onChange={(e) => setNumTunes(e.target.value)} onKeyDown={handleKeyDownFetch}/>
+            <button onClick={() => fetchFromSession(numTunes, setNumTunes, tuneLibrary, setTuneLibrary, remainingTunes, setRemainingTunes)}>Go!</button>
         </div>
         
         </div>
