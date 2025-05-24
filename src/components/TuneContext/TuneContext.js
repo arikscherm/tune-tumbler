bc019144af3cc8a31c9451b39dcbaefcd0458314
arikscherm <arikscherm@gmail.com>
2025-05-24 13:44:14 -0600

    Updating default tune library

--- src/components/TuneContext/TuneContext.js
+++ src/components/TuneContext/TuneContext.js
@@ -5,11 +5,11 @@ export const TuneContext = createContext();
 export const TuneProvider = ({ children }) => {
     const [tuneLibrary, setTuneLibrary] = useState(
       [
-        'Tom Wards Downfall', 'George Whites Favorite', 'Fox on the Town', 'Green Mountain', 'Reconciliation', 'MacArthur Road', 
-        'Black Pats', 'Musical Priest', 'Green Groves of Erin', 'Sally Gardens', 'Maud Miller', 'The Mountain Road',
-        'Flooded Road to Glenties', 'Martin Wynnes #2', 'Star of Munster', 'Sweeneys Dream', 'Congress', 'Palmers Gate',
-        'Dowds No. 9', 'Farrel O Gara', 'Concertina Reel', 'Beare Island', 'Monoghan Twig', 'Earls Chair',
-        'Julia Delaneys', 'Martin OConnors', 'Foxhunters', 'Father Kellys', 'Farewell to Erin', 'Castle Kellys',
+        'Tom Wards Downfall', 'John Keith Lang', 'Fox on the Town', 'Noon Lasses', 'Reconciliation', 'MacArthur Road', 
+        'Black Pats', 'Graf Spee', 'Green Groves of Erin', 'The Road to Errogie', 'Maud Miller', 'The Mountain Road',
+        'Flooded Road to Glenties', 'Martin Wynnes #1', 'Martin Wynnes #2', 'Sweeneys Dream', 'Franks Reel', 'Palmers Gate',
+        'Dowds No 9', 'Farrel OGara', 'Concertina Reel', 'Beare Island', 'Monoghan Twig', 'The Boys on the 25',
+        'The Girl That Broke My Heart', 'Martin OConnors', 'Foxhunters', 'Jerry OSullivans', 'Mouth of the Tobique', 'Music For a Found Harmonium',
       ]);
     const [remainingTunes, setRemainingTunes] = useState([...tuneLibrary]);
     const [setsPlayed, setSetsPlayed] = useState([]);
